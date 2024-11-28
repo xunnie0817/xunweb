f7cfcd76b915693375d95115718135a9fcf2533c
xunnie0817 <asd321321549@gmail.com>
2024-11-28 14:01:20 +0800

    Update 大型網站.js

--- 大型網站.js
+++ 大型網站.js
@@ -138,6 +138,9 @@ else{
             history.style.maxHeight=null;
         }
         else{
+            intro.style.display='block';
+            product.style.display='block';
+            history.style.display='block';
             intro.style.maxHeight='1.5rem';
             product.style.maxHeight='1.5rem';
             history.style.maxHeight='1.5rem';
